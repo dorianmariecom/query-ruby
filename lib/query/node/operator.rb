6265bb89410271670c7e1147a190edf8aa47841c
Dorian Marié <dorian@dorianmarie.com>
2025-04-26 16:47:45 +0200

    add negative prefix to operators

--- lib/query/node/operator.rb
+++ lib/query/node/operator.rb
@@ -3,7 +3,14 @@
 class Query
   class Node
     class Operator < Node
-      attr_accessor :operator
+      attr_accessor :prefix, :operator
+
+      PREFIXES = {
+        "+" => "",
+        "-" => "!",
+        "!" => "!",
+        "" => "",
+      }
 
       OPERATORS = {
         "^" => "^",
@@ -24,11 +31,12 @@ class Query
       }
 
       def initialize(parsed)
-        self.operator = parsed
+        self.prefix = parsed.delete(:prefix)
+        self.operator = parsed.delete(:operator)
       end
 
       def evaluate(**args)
-        OPERATORS.fetch(operator)
+        "#{PREFIXES.fetch(prefix)}#{OPERATORS.fetch(operator)}"
       end
     end
   end
--- lib/query/parser/operator.rb
+++ lib/query/parser/operator.rb
@@ -3,6 +3,13 @@
 class Query
   class Parser
     class Operator < Language
+      PREFIXES = [
+        "-",
+        "+",
+        "!",
+        ""
+      ]
+
       OPERATORS = [
         "^",
         "$",
@@ -12,7 +19,7 @@ class Query
         "=<",
         "<",
         ">",
-        "!",
+        "!"
       ]
 
       MODIFIERS = [
@@ -26,16 +33,19 @@ class Query
       ]
 
       def root
-        OPERATORS.flat_map do |operator|
-          MODIFIERS.map do |modifier|
-            str("#{operator}#{modifier}") |
-              str("#{modifier}#{operator}") |
-              str(operator)
-          end.reduce(&:|).then { operator }
-        end.reduce(&:|) |
-          MODIFIERS.compact_blank.map do |modifier|
-            str(modifier)
-          end.reduce(&:|)
+        PREFIXES.map { |prefix| str(prefix) }.reduce(&:|).aka(:prefix) <<
+        (
+          OPERATORS.flat_map do |operator|
+            MODIFIERS.map do |modifier|
+              str("#{operator}#{modifier}") |
+                str("#{modifier}#{operator}") |
+                str(operator)
+            end.reduce(&:|).then { operator }
+          end.reduce(&:|) |
+            MODIFIERS.compact_blank.map do |modifier|
+              str(modifier)
+            end.reduce(&:|)
+        ).aka(:operator)
       end
     end
   end
